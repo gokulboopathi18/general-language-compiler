04ffffb97b712205ec43f100a61cb02c831e58f2
Prithivi Maruthachalam <prithivimaruthachalam@gmail.com>
2022-01-11 12:49:23 +0530

    warnings supress

--- c_out.c
+++ c_out.c
@@ -4,7 +4,34 @@
 #include<strings.h>
 
 int main () {
-printf("%d",id1);
+printf("வணக்கம் - இரு நிலை எண்கள் பாடம்");
+
+int id0 = 60;
+int id1 = 13;
+int id2 = 0;
+id2 = id0&id1;
+
+printf("Line 1 - மதிப்பு of இ is %d\n",id2);
+
+id2 = id0|id1;
+
+printf("Line 2 - மதிப்பு of இ is %d\n",id2);
+
+id2 = id0^id1;
+
+printf("Line 3 - மதிப்பு of இ is %d\n",id2);
+
+id2 = ~id0;
+
+printf("Line 4 - மதிப்பு of இ is %d\n",id2);
+
+id2 = id0<<2;
+
+printf("Line 5 - மதிப்பு of இ is %d\n",id2);
+
+id2 = id0>>2;
+
+printf("Line 6 - மதிப்பு of இ is %d\n",id2);
 
 return 0;
 }
